01e3775955abd0fba6d07dc1ef5bffb8da29ed9d
Sowmys18 <98528062+Sowmys18@users.noreply.github.com>
2022-07-22 22:58:55 +0530

    Add files via upload

---
+++ Employee-Management-System-with-Spring-Security/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into employee values (1001,'rayan@gmail.com','Rayan','Senta');
+insert into employee values (1002,'mani@gmail.com','Mani','Deva');
+insert into employee values (1003,'sneha@gmail.com','Sneha','Diwari');
+insert into employee values (1004,'rani@gmail.com','Rani','Baviya');
+insert into employee values (1005,'krish@gmail.com','Krish','Navi');
+
+
+--insert into role values (2,'ADMIN');
+--insert into user values (1,'12345','Sowmy');
+--insert into users_roles values (1,2);
---
+++ Employee-Management-System-with-Spring-Security/target/classes/data.sql
@@ -0,0 +1,10 @@
+insert into employee values (1001,'rayan@gmail.com','Rayan','Sengupta');
+insert into employee values (1002,'mani@gmail.com','Mani','Sadhu');
+insert into employee values (1003,'sneha@gmail.com','Sneha','Kumari');
+insert into employee values (1004,'rani@gmail.com','Rani','Baviya');
+insert into employee values (1005,'krish@gmail.com','Krish','Dutta');
+
+
+--insert into role values (2,'ADMIN');
+--insert into user values (1,'12345','Sowmy');
+--insert into users_roles values (1,2);
